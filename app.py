4b755bb3ec686a4bc3014f52a49a8c55f68ab375
🥶Nilmar 🥶 <danielnilmar267@gmail.com>
2026-05-21 11:57:28 -0300

    Add files via upload
    1°Versão do site

--- app.py
+++ app.py
@@ -1,60 +1,55 @@
 import streamlit as st
 
-# python -m streamlit run app.py
+st.title("🚗Nilmar Car Rental")  
+st.title(" -Alugue seu carro- ")
 
-# ------------------------------------------------- Sidebar
-
+st.sidebar.title("Escolhar o seu modelo")
 st.sidebar.image("logo.png")
-st.sidebar.title('Mateus Motors')
 
+st.sidebar.title("CARROS e MOTOS")
+carros = ["M2 BMW","GOLF GTI","CIVIC G10","OPALA","SW4","S1000RR","FAN 160","BMW r1250 GS","africa twin"]
 
-carros = ['BMW','Mustang', 'Porsche', 'Fusca', 'Toro']
-
-opcao = st.sidebar.selectbox('Escolha o carro que foi alugado', carros)
-
-
-
-# ----------------------------------------------- Principal 
-st.title('Mateus motors - Aluguel de Carros')
+opcao = st.sidebar.selectbox(' o carro ou moto que foi alugado', carros)
 
 st.image(f'{opcao}.png')
-st.markdown(f'## Você alugou o modelo: {opcao}')
+st.markdown(f'## você alugou o modelo: {opcao}')
 st.markdown('---')
+dias = st.text_input(f'Por quantos dias o {opcao} foi alugado? ')
 
-dias = st.text_input(f'Por quantos dias o {opcao} foi alugado?')
-km = st.text_input(f'Quantos km você rodou com o {opcao}?')
+km = st.text_input(f'Quantos Km você rodou com o {opcao}?')
 
-if opcao == 'BMW':
-    diaria = 450
+if opcao in "M2 BMW":
+    diaria = 1500
 
-elif opcao == 'Mustang':
-    diaria = 500
+elif opcao in "GOLF GTI":
+    diaria = 550    
 
-elif opcao == 'Porsche':
+elif opcao in "CIVIC G10":
+    diaria = 1000
+
+elif opcao in "OPALA":  
+    diaria = 700
+
+elif opcao in "SW4":
+    diaria = 650
+
+elif opcao in "S1000RR":
     diaria = 300
+elif opcao in "FAN 160":
+    diaria = 400
 
-elif opcao == 'Fusca':
-    diaria = 250
+elif opcao in "BMW r1250 GS":
+    diaria = 600
 
-elif opcao == 'Toro':
-    diaria = 550
-
-
-
-
+elif opcao in "africa twin":
+    diaria = 500
 
 if st.button('Calcular'):
     dias = int(dias)
-    km = float(km)
+    km = int(km)
 
     total_dias = dias * diaria
     total_km = km * 0.15
     aluguel_total = total_dias+total_km
 
-    st.warning(f'Você alugou o {opcao} por {dias} dias e rodou {km}km. O valor total a pagar é R${aluguel_total:.2f}')
-
-
-
-
-
-
+    st.warning(f'Você alugou o {opcao} por {dias} dias e rodou {km} Km. O valor total do aluguel é de R$ {aluguel_total:.2f}.')
